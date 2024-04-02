daa97d96373cf88e4366e20fea068eda20b077bd
anvimaa <anvimaa@gmail.com>
2024-04-02 15:17:57 +0100

    aupdate dashboard page

--- src/routes/(app)/dashboard/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import { enhance } from "$app/forms";
-  import type { PageData } from "./$types";
-
-  export let data: PageData;
-</script>
-
-<h1>Hi, {data.user.username}!</h1>
